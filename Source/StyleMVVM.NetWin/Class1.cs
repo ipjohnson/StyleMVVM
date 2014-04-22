16c444c5c503ff41e422373dc944e60647a37311
ipjohnson <ipjohnson123@yahoo.com>
2014-04-21 18:52:22 -0600

    Initial commit

---
+++ Source/StyleMVVM.NetWin/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Security;
+using System.Text;
+using System.Windows;
+
+
+namespace StyleMVVM.NetWin
+{
+    public class Class1
+    {
+		private WindowsClientCredential.
+    }
+}
