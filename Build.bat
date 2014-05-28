4dd9dd885b358395dc4ff15dbf29c5c7bc8cc01f
ipjohnson <ipjohnson123@yahoo.com>
2014-05-27 18:02:57 -0600

    build file changes

--- Build.bat
+++ Build.bat
@@ -1,3 +1,3 @@
-CALL "C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\Common7\\Tools\\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\VsDevCmd.bat"
 
 msbuild Source\StyleMVVM.sln /p:Configuration=BuildMachine
