53238c88845bc62b7af5d1f6664c155cc2ee085d
ipjohnson <ipjohnson123@yahoo.com>
2014-06-09 19:07:44 -0600

    Conventions changes

--- Source/StyleMVVM/View/Conventions.cs
+++ Source/StyleMVVM/View/Conventions.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Grace.DependencyInjection;
+using StyleMVVM.Ultilities;
 #if NETFX_CORE
 using Windows.UI.Xaml;
 #else
@@ -51,14 +52,31 @@ namespace StyleMVVM.View
 		private static void BindAtDesignTimeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
 		{
 			bool designTime = (bool)e.NewValue;
+			FrameworkElement element = d as FrameworkElement;
 
-			if(designTime)
+			if (element == null || !DesignModeUtility.DesignModeIsEnabled)
 			{
+				return;
+			}
 
+			if (designTime)
+			{
+				IExportLocator exportLocator = DesignTimeBootstrapper.GetDesignTimeExportLocatorInstance(element);
+
+				IViewBinderService viewBinderService = exportLocator.Locate<IViewBinderService>();
+
+				if (viewBinderService != null)
+				{
+					viewBinderService.Bind(element, null);
+				}
+				else
+				{
+					throw new Exception("Could not locate IViewBinderService");
+				}
 			}
 			else
 			{
-
+				element.DataContext = null;
 			}
 		}
 	}
